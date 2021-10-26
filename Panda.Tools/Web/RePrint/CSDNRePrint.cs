d9586ce2f5b161f3162fdc96544db0b6709b26ad
qingcheng <qingchengcode@qq.com>
2021-10-27 00:06:21 +0800

    update

---
+++ Panda.Tools/Web/RePrint/CSDNRePrint.cs
@@ -0,0 +1,9 @@
+namespace Panda.Tools.Web.RePrint;
+
+public class CSDNRePrint:IRePrint
+{
+    public async Task<RePrintPostModel> RePrint(string url)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Panda.Tools/Web/RePrint/RePrintFactory.cs
+++ Panda.Tools/Web/RePrint/RePrintFactory.cs
@@ -1,12 +1,25 @@
+using System.Text.RegularExpressions;
+
 namespace Panda.Tools.Web.RePrint;
 
 public class RePrintFactory
 {
     public async Task<RePrintPostModel> RePrint(string url)
     {
-        var model = new RePrintPostModel();
-        IRePrint rePrint = new CNBlogRePrint();
-        var res =  await rePrint.RePrint(url);
+        var matchRes =  Regex.Match(url, @"(?<=\/\/).*?(?=\/)");
+        IRePrint rePrint = null;
+        switch (matchRes.Value)
+        {
+            case "blog.csdn.net":
+                Console.WriteLine("csdn");
+                rePrint = new CSDNRePrint();
+                break;
+            case "www.cnblogs.com":
+                Console.WriteLine("cnblog");
+                rePrint = new CNBlogRePrint();
+                break;
+        }
+        var res =  await rePrint?.RePrint(url)!;
         return res;
     }
 }
