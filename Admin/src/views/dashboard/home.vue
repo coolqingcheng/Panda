f1280c39986beec318bf461139ad86d0d59cbfd6
qingcheng <qingchengcode@qq.com>
2021-11-16 23:07:26 +0800

    update

--- Admin/src/views/dashboard/home.vue
+++ Admin/src/views/dashboard/home.vue
@@ -1,16 +1,43 @@
+
 <template>
   <el-card>
-      <h1>Home</h1>
-      <cropper-box></cropper-box>
+    <h1 @click="test()">Home {{ visible }}</h1>
+    <cropper-box
+      v-model="visible"
+      @cropper="close"
+      url="https://img2.baidu.com/it/u=1232855683,1763177858&fm=253&fmt=auto&app=138&f=PNG?w=499&h=270"
+    ></cropper-box>
+    <img :src="url" />
   </el-card>
 </template>
 
-<script>
+<script lang="ts">
+import { ref } from 'vue'
 export default {
+  setup() {
+    const visible = ref(true)
 
+    const url = ref('')
+
+    const test = () => {
+      visible.value = !visible.value
+    }
+
+    const close = (e: { base64: string }) => {
+      // visible.value = false
+      console.log(e)
+      url.value = e.base64
+
+    }
+    return {
+      visible,
+      test,
+      close,
+      url
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
