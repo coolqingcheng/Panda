290a832f775075a9f16b715ecb3b610fd0185477
qingcheng <qingchengcode@qq.com>
2022-03-12 00:20:18 +0800

    update

--- Panda.Tools/Auth/AppContext.cs
+++ Panda.Tools/Auth/AppContext.cs
@@ -13,4 +13,54 @@ public abstract class AppContext<TU> : DbContext where TU : Accounts
     {
         
     }
+    
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        SaveChangeModifyAddTime();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public override int SaveChanges()
+    {
+        SaveChangeModifyAddTime();
+        return base.SaveChanges();
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        SaveChangeModifyAddTime();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        SaveChangeModifyAddTime();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void SaveChangeModifyAddTime()
+    {
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            if (entry.State == EntityState.Modified)
+            {
+                if (!entry.CurrentValues.TryGetValue<DateTimeOffset>("UpdateTime", out var time)) continue;
+                if (time == default)
+                {
+                    entry.CurrentValues["UpdateTime"] = DateTimeOffset.Now;
+                }
+            }
+
+            if (entry.State == EntityState.Added)
+            {
+                if (!entry.CurrentValues.TryGetValue<DateTimeOffset>("AddTime", out var time)) continue;
+                if (time == default)
+                {
+                    entry.CurrentValues["AddTime"] = DateTimeOffset.Now;
+                }
+            }
+        }
+    }
 }
