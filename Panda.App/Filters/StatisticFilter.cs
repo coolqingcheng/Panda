5ca83c7144c604a8addb4116f85ecd03f4434457
qingcheng <qingchengcode@qq.com>
2022-04-29 17:30:34 +0800

    update

---
+++ Panda.App/Filters/StatisticFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Panda.Tools.QueueTask;
+
+namespace Panda.App;
+
+public class StatisticFilter : IAsyncPageFilter
+{
+    private readonly QueueTaskManager _queueTaskManager;
+
+    public StatisticFilter(QueueTaskManager queueTaskManager)
+    {
+        _queueTaskManager = queueTaskManager;
+    }
+
+    public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+    {
+        _queueTaskManager.Run(serviceProvider =>
+        {
+            
+        });
+        return  Task.CompletedTask;
+    }
+
+    public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+    {
+        await next.Invoke();
+    }
+}
--- Panda.Tools/QueueTask/QueueTaskManager.cs
+++ Panda.Tools/QueueTask/QueueTaskManager.cs
@@ -5,11 +5,11 @@ namespace Panda.Tools.QueueTask;
 
 public class QueueTaskManager : IDisposable
 {
-    private static ConcurrentQueue<Action<IServiceProvider>> Queue { get; set; } = new();
+    private ConcurrentQueue<Action<IServiceProvider>> Queue { get; set; } = new();
 
-    private static bool _isRun = true;
+    private bool _isRun = true;
 
-    public static void Run(Action<IServiceProvider> action)
+    public void Run(Action<IServiceProvider> action)
     {
         Queue.Enqueue(action);
     }
@@ -34,6 +34,7 @@ public class QueueTaskManager : IDisposable
     public void Dispose()
     {
         // todo 现在直接扔掉，不处理队列的数据
+        Queue.Clear();
         _isRun = false;
     }
 }
